0ab4c7dabd1b78ad8f0b5d8dd4bde1459a770e15
grs-dw <guillaume.rams@deveryware.com>
2014-03-21 17:46:48 +0100

    Now using GPP macro processor instead of cpp

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,12 @@ USER_ID=$(<${TOPDIR}/baselines/UserId)
 echo Building $USER_ID/$IMG_NAME...
 echo 
 # -C is required not to interpret // as a comment to be removed. Sadly, /* is still an issue... 
-cpp -C -I${TOPDIR} Dockerfile.in -o Dockerfile $@
+#cpp -C -I${TOPDIR} Dockerfile.in -o Dockerfile $@
+gpp -U "#" "" "(" "," ")" "(" ")" "#" "°" -M "#" "\n" " " " " "\n" "(" ")" -n +c "##" "\n" -I${TOPDIR} Dockerfile.in -o Dockerfile
 if [ $? -gt 0 ]; then
     exit 1
 fi
-docker build --rm=true --tag="$USER_ID/$IMG_NAME" .
+docker build --rm=true --tag="$USER_ID/$IMG_NAME" $@ .
 if [ $? -gt 0 ]; then
     exit 1
 fi
